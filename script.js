d57200d24cc1726549901e15e0ef54397dc8fa80
Floogle <18466542+skyfloogle@users.noreply.github.com>
2025-01-06 22:36:04 +0100

    improve

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const tab_divs = document.getElementsByClassName("tabdiv");
 const tab_bar = document.getElementById("Tabs");
 for (let i = 0; i < tab_divs.length; i++) {
-    var button = document.createElement("button");
-    button.className="tab";
+    var button = document.createElement("a");
+    button.className="button tab";
+	button.href="#"+tab_divs[i].id;
     button.onclick=function(){select_tab(this);};
     button.innerHTML='<img class="buticon" src="images/tabs_'+tab_divs[i].id+'.png"/>'+tab_divs[i].id;
     tab_bar.appendChild(button);
@@ -24,31 +25,10 @@ if (focustab) {
 function select_tab(self) {
     window.location.hash = "#"+self.innerText;
     var last = (document.getElementsByClassName("activetab"))[0];
-    if (last) last.className="tab";
-    self.className="activetab";
+    if (last) last.className="button tab";
+    self.className="button activetab";
     for (let i = 0; i < tab_divs.length; i++) {
         if (tab_divs[i].id == self.innerText) tab_divs[i].style.display = "block";
         else tab_divs[i].style.display = "none";
     }
-}
-
-
-//button links
-function new_tab(url){
-    var link=document.createElement('a');
-    Object.assign(link,{target:'_blank',href:url}).click();
-    link.remove();
-}
-
-
-//documentation buttons
-const doc_iframe = document.getElementById("documentation");
-function doc_back() {
-    window.history.back();
-}
-function doc_fwd() {
-    window.history.forward();
-}
-function doc_home() {
-    doc_iframe.src=doc_iframe.src;
 }
